b75caa2d6dbc431253c6ed743c33b1d10e46ae94
ashikprodhan2006 <ashikprodhan2006@gmail.com>
2025-04-25 15:14:13 +0600

    CartViewset updated for create or get cart

--- order/views.py
+++ order/views.py
@@ -9,6 +9,7 @@ from product.permissions import IsReviewAuthorOrReadonly
 from rest_framework.decorators import action
 from order.services import OrderService
 from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
@@ -124,8 +125,19 @@ class OrderViewset(ModelViewSet):
         if getattr(self, 'swagger_fake_view', False):
             return Order.objects.none()
         
-        if self.request.user.is_staff:
-        #     return Order.objects.all()
-        # return Order.objects.filter(user=self.request.user)
-            return Order.objects.prefetch_related('items__product').all()
+        # if self.request.user.is_staff:
+        # #     return Order.objects.all()
+        # # return Order.objects.filter(user=self.request.user)
+        #     return Order.objects.prefetch_related('items__product').all()
         return Order.objects.prefetch_related('items__product').filter(user=self.request.user)
+
+
+    def create(self, request, *args, **kwargs):
+        existing_cart = Cart.objects.filter(user=request.user).first()
+
+        if existing_cart:
+            serializer = self.getserializer(existing_cart)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return super().create(request, *args, **kwargs)
